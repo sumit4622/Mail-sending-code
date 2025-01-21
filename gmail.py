1f5d57d703fd3ff4a2a7a27385a35430912a4900
Sumit Ray <133256850+sumit4622@users.noreply.github.com>
2025-01-21 19:38:38 +0545

    adding some useful comment to gmail.py

--- gmail.py
+++ gmail.py
@@ -1,25 +1,36 @@
 import smtplib, ssl
 
 
-class MailClient:    
+class MailClient:
+    """
+    A class to simplify sending emails using Gmail's SMTP server.
+    """
+
     smtp_server = "smtp.gmail.com"
     port = 465
     sender_email = "aitmsams@gmail.com"
-    receiver_email = "sumit.ray9299@gmail.com"
-    password = "huha dmqx lrkj wuac"
+    receiver_email = "sum****ay9299@gmail.com"
+    password = "huha **** **** wuac"  # Please replace with your password!
     subject = ""
     body = ""
 
-
     def __init__(self):
+        """
+        Creates a new MailClient object and creates a secure SSL context for sending emails.
+        """
         self.context = ssl.create_default_context()
 
-
     def send(self):
+        """
+        Sends an email with the specified subject and body to the recipient email address.
+
+        Raises:
+            Exception: If there is an error while sending the email.
+        """
         try:
             message = f"Subject: {self.subject}\n\n{self.body}"
             with smtplib.SMTP_SSL(self.smtp_server, self.port, context=self.context) as server:
-                server.ehlo()  
+                server.ehlo()
                 server.login(self.sender_email, self.password)
                 server.sendmail(self.sender_email, self.receiver_email, message)
         except Exception as e:
